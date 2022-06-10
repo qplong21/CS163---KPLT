dcde75b17d97e0e4ed417712e337439b48d34978
viettungvuong <viettungvuong@gmail.com>
2022-06-10 11:46:00 +0700

    Dung async load to linked list

--- CS163 - KPLT/CS163 - KPLT.cpp
+++ CS163 - KPLT/CS163 - KPLT.cpp
@@ -5,20 +5,10 @@ int main()
 {
     TernarySearchTree arr_of_tree;
     arr_of_tree.import_dictionary();
-<<<<<<< HEAD
-    while (1)
-    {
-        std::string s;
-        std::getline(std::cin, s);
-        if (s == "")
-        {
-=======
-    std::cout << "done";
     while (1) {
         std::string s;
         std::getline(std::cin, s);
         if (s == "")
->>>>>>> 74b91c22155454e4a437ba654cb08f394dc792b8
             break;
         arr_of_tree.search4keyword(s);
     }
