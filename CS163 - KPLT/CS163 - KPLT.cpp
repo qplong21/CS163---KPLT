5183c5137c77985e49ae54136dde31946b57a7da
qplong21 <100032289+qplong21@users.noreply.github.com>
2022-06-23 08:19:31 +0700

    Fix save function
    and fix the penis size XD

--- CS163 - KPLT/CS163 - KPLT.cpp
+++ CS163 - KPLT/CS163 - KPLT.cpp
@@ -6,7 +6,7 @@ int main()
 	srand(time(NULL)); //khoi tao random//Tung dang test cai nay
 	TernarySearchTree listOfTree[NUMofSET];
 	createSet(listOfTree);//khoi tao
-
+	
 	saveAllTree(listOfTree);
 	
 	for (int i = 0; i < 3; ++i)//xoa
