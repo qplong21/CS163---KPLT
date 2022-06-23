af927ef4a8b5e061498a5c4b82507200f9a8862d
Tung <viettungvuong@gmail.com>
2022-06-23 22:41:37 +0700

    Them ham test

--- CS163 - KPLT/CS163 - KPLT.cpp
+++ CS163 - KPLT/CS163 - KPLT.cpp
@@ -6,9 +6,14 @@ int main()
 	srand(time(NULL)); //khoi tao random//Tung dang test cai nay
 	TernarySearchTree listOfTree[NUMofSET];
 	createSet(listOfTree);//khoi tao
-	
+
 	saveAllTree(listOfTree);
-	
+
+
+	listOfTree[2].search4keyword("Apple");
+	listOfTree[2].addNewWordToDict();
+	listOfTree[2].search4keyword("Apple");
+
 	for (int i = 0; i < 3; ++i)//xoa
 	{
 		listOfTree[i].deleteTree();
