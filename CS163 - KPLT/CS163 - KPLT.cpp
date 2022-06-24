3b97bf6ec6fdbbd8fa649291cbc223d4b824d0bb
qplong21 <100032289+qplong21@users.noreply.github.com>
2022-06-24 10:18:16 +0700

    push

--- CS163 - KPLT/CS163 - KPLT.cpp
+++ CS163 - KPLT/CS163 - KPLT.cpp
@@ -4,15 +4,13 @@
 int main()
 {
 	header();
-	system("pause");
+	//system("pause");
 	srand(time(NULL)); //khoi tao random//Tung dang test cai nay
 	TernarySearchTree listOfTree[NUMofSET];
 	createSet(listOfTree);//khoi tao
 
-	listOfTree[2].search4keyword("Apple");
-	listOfTree[2].addNewWordToDict();
-	listOfTree[2].search4keyword("Apple");
-	saveAllTree(listOfTree);
+
+	resetAllDictionary(listOfTree);
 	for (int i = 0; i < 3; ++i)//xoa
 	{
 		listOfTree[i].deleteTree();
