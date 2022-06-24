00c766bc93eec695728a6918c593af5445ba8d22
VThanhPhuc <99302070+VThanhPhuc@users.noreply.github.com>
2022-06-24 10:02:46 +0700

    update Header for app

--- CS163 - KPLT/Screen.cpp
+++ CS163 - KPLT/Screen.cpp
@@ -5,7 +5,7 @@ void header() {
 	SetBGColor(15);
 	system("cls");
 	SetColor(1);
-	gotoxy(7, 0);
+	gotoxy(15, 0);
 	std::cout << "Welcome to Dictionary";
 	gotoxy(12, 1);
 	std::cout << "Made by Group 4-KPLT-APCS21";
